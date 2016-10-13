ae19281e0ad1f53bbb3e6c6b4836a9e3c0459949
amit <shettya@gmail.com>
2016-10-13 14:43:30 -0700

    replace const array with json data

--- js/main.jsx
+++ js/main.jsx
@@ -37,21 +37,34 @@ class LeaderBoardData extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			data: lbData
+			data: []
 		};
 	}
+	componentDidMount(){
+		$.ajax({
+			url: 'https://fcctop100.herokuapp.com/api/fccusers/top/recent',
+			success: (data)=>{
+				this.setState({
+					data: data
+				});
+			},
+			error: function(error){
+				console.log(error);
+			}
+		})
+	}
+
 	render(){
 		return(
 			<div>
 				{this.state.data.map((userInfo, index)=>{
 					return(
-						<div className="dataRow text-center">
+						<div className="dataRow text-center" key={index}>
 							<div className="colNumber">{index+1}</div>
 							<div className="colName">{userInfo.username}</div>
 							<div className="colPoints">{userInfo.recent}</div>
 							<div className="colTotalPoints">{userInfo.alltime}</div>
-						</div>
-						
+						</div>						
 						);
 				})}
 			</div>
